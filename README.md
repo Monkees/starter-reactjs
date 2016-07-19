3169b60f06bdb0d7d3efb3a062cfef8bd5ec9c91
rkueny <raphael@kueny.fr>
2016-07-19 17:23:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Starter ReactJs
+===============
+
+[From CodeMentor.io](https://www.codementor.io/reactjs/tutorial/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack)
+
+Run project :
+
+- `npm install`
+- `npm run dev`
+- Go to src/client/index.html in your browser
---
+++ src/app/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AwesomeComponent from './components/TestComponent.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <p> Hello React!</p>
+        <AwesomeComponent />
+      </div>
+    );
+  }
+}
+
+render (
+  <App/>,
+  document.getElementById('app')
+);
